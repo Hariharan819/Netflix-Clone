49c3bd2b679f2018e241d96509691b13026d7633
Hariharan <hariharanvenkatachalam11@gmail.com>
2024-08-05 13:56:57 +0530

    the ytkey ifo is ctored in redux

--- src/Components/VideoBackgroud.js
+++ src/Components/VideoBackgroud.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { API_OPTIONS } from "../Utilis/constant";
+import { useDispatch, useSelector } from "react-redux";
+import { addmovieinfo } from "../ReduxUtils/movieslice";
 
 const VideoBackgroud = ({ movieid }) => {
-  const [ytkey, setytkey] = useState(null);
+  //   const [ytkey, setytkey] = useState(null);
+  const dispatch = useDispatch();
+  const ytkeyfromreduxstore = useSelector(
+    (store) => store?.movies?.trailerinfo
+  );
   const moviefetchapi = async () => {
     const data = await fetch(
       `https://api.themoviedb.org/3/movie/${movieid}/videos?language=en-US`,
@@ -11,7 +17,8 @@ const VideoBackgroud = ({ movieid }) => {
     const json = await data.json();
     const trailer = json.results.filter((videos) => videos.type === "Trailer");
     const filtertrailer = trailer.length ? trailer[0] : json.results[0];
-    setytkey(filtertrailer.key);
+    // setytkey(filtertrailer.key);
+    dispatch(addmovieinfo(filtertrailer));
   };
   useEffect(() => {
     moviefetchapi();
@@ -21,7 +28,7 @@ const VideoBackgroud = ({ movieid }) => {
       <iframe
         width="560"
         height="315"
-        src={"https://www.youtube.com/embed/" + ytkey}
+        src={"https://www.youtube.com/embed/" + ytkeyfromreduxstore?.key}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
--- src/ReduxUtils/movieslice.js
+++ src/ReduxUtils/movieslice.js
@@ -1,16 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const movieslice=createSlice({
-    name:"movies",
-    initialState:{
-        movieslist:null, 
+const movieslice = createSlice({
+  name: "movies",
+  initialState: {
+    movieslist: null,
+    trailerinfo: null,
+  },
+  reducers: {
+    addmovieslist: (state, action) => {
+      state.movieslist = action.payload;
     },
-    reducers:{
-        addmovieslist:(state,action)=>{
-            state.movieslist=action.payload;
-        },
+    addmovieinfo: (state, action) => {
+      state.trailerinfo = action.payload;
     },
+  },
 });
 
-export const {addmovieslist}=movieslice.actions;
+export const { addmovieslist, addmovieinfo } = movieslice.actions;
 export default movieslice.reducer;
